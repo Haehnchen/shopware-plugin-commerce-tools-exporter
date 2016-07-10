c98eaa86d4d9bc3d0e2246520c48f07baff42dcd
Daniel Espendiller <daniel@espendiller.net>
2016-07-10 12:44:19 +0200

    dropping old plugin skeleton stuff

--- CommerceToolsExporter.php
+++ CommerceToolsExporter.php
@@ -4,33 +4,12 @@ namespace CommerceToolsExporter;
 
 use CommerceToolsExporter\Commands\CheckCommand;
 use CommerceToolsExporter\Commands\ExportCommand;
-use Doctrine\Common\Collections\ArrayCollection;
 use Enlight\Event\SubscriberInterface;
 use Shopware\Components\Console\Application;
 use Shopware\Components\Plugin;
-use Shopware\Components\Plugin\Context\InstallContext;
 
 class CommerceToolsExporter extends Plugin implements SubscriberInterface
 {
-    public static function getSubscribedEvents()
-    {
-        return [
-            'Shopware_Console_Add_Command' => 'onAddConsoleCommand',
-        ];
-    }
-
-    public function onAddConsoleCommand(\Enlight_Event_EventArgs $args)
-    {
-        return new ArrayCollection([
-            new ExportCommand(),
-        ]);
-    }
-    
-    public function install(InstallContext $context)
-    {
-        
-    }
-
     public function registerCommands(Application $application)
     {
         // @TODO: wait for accepted pull request of symfony tag
